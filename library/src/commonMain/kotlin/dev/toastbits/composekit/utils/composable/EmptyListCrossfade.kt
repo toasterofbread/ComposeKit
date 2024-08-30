cab90b97c2b4616ec6ee058d452b9ff8b3e50368
toasterofbread <talohalton@gmail.com>
2024-08-31 00:48:48 +0100

    Fix NullableValueAnimatedVisibility not animating on first appearance

--- library/src/commonMain/kotlin/dev/toastbits/composekit/utils/composable/EmptyListCrossfade.kt
+++ library/src/commonMain/kotlin/dev/toastbits/composekit/utils/composable/EmptyListCrossfade.kt
@@ -1,7 +1,17 @@
 package dev.toastbits.composekit.utils.composable
 
-import androidx.compose.animation.*
-import androidx.compose.runtime.*
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -68,14 +78,17 @@ fun <T: Any> NullableValueAnimatedVisibility(
     content: @Composable (value: T?) -> Unit
 ) {
     var current_value: T? by remember { mutableStateOf(value) }
+    var show: Boolean by remember { mutableStateOf(value != null) }
+
     LaunchedEffect(value) {
         if (value != null) {
             current_value = value
         }
+        show = value != null
     }
 
     AnimatedVisibility(
-        value != null,
+        show,
         modifier,
         enter = enter,
         exit = exit
