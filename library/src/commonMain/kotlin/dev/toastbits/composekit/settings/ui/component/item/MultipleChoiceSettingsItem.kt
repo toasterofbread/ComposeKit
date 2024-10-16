25ce30f1258ec5334681d07a9e00cb0d1e80258d
toasterofbread <talohalton@gmail.com>
2024-10-16 09:11:45 +0100

    Restyle MultipleChoiceSettingsItem

--- library/src/commonMain/kotlin/dev/toastbits/composekit/settings/ui/component/item/MultipleChoiceSettingsItem.kt
+++ library/src/commonMain/kotlin/dev/toastbits/composekit/settings/ui/component/item/MultipleChoiceSettingsItem.kt
@@ -1,32 +1,25 @@
 package dev.toastbits.composekit.settings.ui.component.item
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.toastbits.composekit.platform.PreferencesProperty
 import dev.toastbits.composekit.platform.composable.theme.LocalApplicationTheme
 import dev.toastbits.composekit.settings.ui.ThemeValues
-import dev.toastbits.composekit.settings.ui.vibrant_accent
-import dev.toastbits.composekit.settings.ui.on_accent
-import dev.toastbits.composekit.utils.composable.WidthShrinkText
 
 class MultipleChoiceSettingsItem(
     val state: PreferencesProperty<Int>,
@@ -53,32 +46,29 @@ class MultipleChoiceSettingsItem(
 
                 Spacer(Modifier.height(10.dp))
 
-                Column(Modifier.padding(start = 15.dp), verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                Column(
+                    Modifier.padding(start = 15.dp)
+                ) {
                     for (i in 0 until choice_amount) {
-                        val colour: Color by animateColorAsState(if (current_value == i) theme.vibrant_accent else Color.Transparent)
 
-                        Box(
-                            contentAlignment = Alignment.CenterStart,
-                            modifier = Modifier
-                                .border(
-                                    Dp.Hairline,
-                                    theme.on_background,
-                                    SETTINGS_ITEM_ROUNDED_SHAPE
-                                )
-                                .fillMaxWidth()
-                                .height(40.dp)
+                        Row(
+                            Modifier
                                 .clickable(remember { MutableInteractionSource() }, null) {
                                     state.set(i)
-                                }
-                                .background(colour, SETTINGS_ITEM_ROUNDED_SHAPE)
+                                },
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
                         ) {
-                            Box(Modifier.padding(horizontal = 10.dp)) {
-                                WidthShrinkText(
-                                    getChoiceText(i),
-                                    style = LocalTextStyle.current.copy(color = if (current_value == i) theme.on_accent else theme.on_background),
-                                    modifier = Modifier.fillMaxWidth()
-                                )
-                            }
+                            Text(
+                                getChoiceText(i),
+                                color = theme.on_background.copy(alpha = 0.8f),
+                                modifier = Modifier.fillMaxWidth().weight(1f)
+                            )
+
+                            RadioButton(
+                                selected = current_value == i,
+                                onClick = { state.set(i) }
+                            )
                         }
                     }
                 }
